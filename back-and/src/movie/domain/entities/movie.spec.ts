2f413e8232974f2e89eafbee0eb0067ac8887524
DavidFune] <david.funy28@gmail.com>
2022-11-28 11:50:45 -0300

    main: defining movie class structure

--- back-and/src/movie/domain/entities/movie.spec.ts
+++ back-and/src/movie/domain/entities/movie.spec.ts
@@ -1,9 +1,28 @@
 import { Movie } from "./movie";
-
+import { validate as uuidValidate } from "uuid";
 describe('Movie Test', () => {
 
     test('Construvt of movie', () => {
-        const movie: Movie = new Movie("Castle in the Sky", "banner.jpg", "The orphan Sheeta inherited", "Isao Takahata", "Hayao Miyazaki")
-        expect(movie.title).toBe("Castle in the Sky")
+        const movie: Movie = new Movie({
+        title :"Castle in the Sky", 
+        banner :"banner.jpg", 
+        description :"The orphan Sheeta inherited", 
+        producer :"Isao Takahata", 
+        director :"Hayao Miyazaki"})
+        
+        expect(movie.props.title).toBe("Castle in the Sky")
     });
+
+    test('Validade Id', () => {
+        const movie: Movie = new Movie({
+            title :"Castle in the Sky", 
+            banner :"banner.jpg", 
+            description :"The orphan Sheeta inherited", 
+            producer :"Isao Takahata", 
+            director :"Hayao Miyazaki"})
+
+            expect(movie.id).not.toBeNull()
+            expect(uuidValidate(movie.id)).toBeTruthy()
+    });
+
 });
--- back-and/src/movie/domain/entities/movie.ts
+++ back-and/src/movie/domain/entities/movie.ts
@@ -1,9 +1,20 @@
+import { v4 as uuidv4 } from "uuid";
+export type MovieProps = {
+    title: string,
+    banner: string,
+    description: string,
+    producer: string,
+    director: string
+}
+
 export class Movie {
-    constructor(
-        public title: string, 
-        public banner: string, 
-        public description: string, 
-        public producer: string, 
-        public director: string
-        ){}
+    public readonly id: string;
+    constructor(public readonly props: MovieProps, id?: string) {
+        this.id = id || uuidv4()
+        this.props.title = props.title
+        this.props.banner = props.banner
+        this.props.description = props.description
+        this.props.producer = props.producer
+        this.props.director = props.director
+    }
 }
