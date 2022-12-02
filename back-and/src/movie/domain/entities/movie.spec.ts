89edd6078cf2f122c759bcb08dd610dc9ac52341
DavidFune] <david.funy28@gmail.com>
2022-12-02 17:11:10 -0300

    main: unit test the update movie

--- back-and/src/movie/domain/entities/movie.spec.ts
+++ back-and/src/movie/domain/entities/movie.spec.ts
@@ -2,6 +2,9 @@ import { Movie } from "./movie";
 import UniqueEntityId from "../../../@seadwork/domain/value-objects/unique-entity-id";
 describe('Movie Test', () => {
 
+    beforeEach( () => {
+        Movie.validate = jest.fn()
+    })
     test('Construvt of movie', () => {
         const movie: Movie = new Movie({
         title :"Castle in the Sky", 
@@ -9,7 +12,7 @@ describe('Movie Test', () => {
         description :"The orphan Sheeta inherited", 
         producer :"Isao Takahata", 
         director :"Hayao Miyazaki"})
-        
+        expect(Movie.validate).toHaveBeenCalled();
         expect(movie.props.title).toBe("Castle in the Sky")
     });
 
@@ -24,5 +27,30 @@ describe('Movie Test', () => {
             expect(movie.id).not.toBeNull()
             expect(movie.uniqueEntityId).toBeInstanceOf(UniqueEntityId)
     });
+
+    it("should update a Movie", () => {
+        const movie: Movie = new Movie({
+            title :"Castle in the Sky", 
+            banner :"banner.jpg", 
+            description :"The orphan Sheeta inherited", 
+            producer :"Isao Takahata", 
+            director :"Hayao Miyazaki"})
+
+        movie.update(
+        "test update",
+        "test update",
+        "test update",
+        "test update",
+        "test update"
+        );
+
+        expect(Movie.validate).toHaveBeenCalledTimes(2);
+        
+        expect(movie.props.title).toBe("test update");
+        expect(movie.props.banner).toBe("test update");
+        expect(movie.props.description).toBe("test update");
+        expect(movie.props.producer).toBe("test update");
+        expect(movie.props.director).toBe("test update");
+      });
 
 });
