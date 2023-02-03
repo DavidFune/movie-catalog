cf0d6623032adb42b0858cfadcf15cbf7abe0f36
DavidFune] <david.funy28@gmail.com>
2023-02-02 21:16:37 -0300

    main: movie repository improvements

--- back-and/src/movie/domain/repository/movie.repository.ts
+++ back-and/src/movie/domain/repository/movie.repository.ts
@@ -1,6 +1,29 @@
-import { SearchableRepositoryInterface } from "../../../@seadwork/domain/repository/repository-contracts";
+import {
+    SearchParams as DefaultSearchParams, 
+    SearchResult as DefaultSearchResult, 
+    SearchableRepositoryInterface } from "../../../@seadwork/domain/repository/repository-contracts";
 import { Movie } from "../entities/movie";
 
-export default interface MovieRepository extends SearchableRepositoryInterface<Movie, any, any>{
+
+export namespace MovieRepository{
+
+    export type Filter = string
     
+    export class SearchParams extends DefaultSearchParams<Filter>{}
+    
+    export class SearchResult extends DefaultSearchResult<
+        Movie,
+        Filter
+    >{}
+    export interface Repository 
+        extends SearchableRepositoryInterface<
+        Movie, 
+        Filter, 
+        SearchParams,
+        SearchResult
+        >{
+        
+    }
 }
+
+export default MovieRepository
--- back-and/src/movie/infra/repository/movie-in-memory.repository.ts
+++ back-and/src/movie/infra/repository/movie-in-memory.repository.ts
@@ -1,10 +1,37 @@
-import {InMemorySearchableRepository} from "../../../@seadwork/domain/repository/in-memory.repository";
+import { InMemorySearchableRepository } from "../../../@seadwork/domain/repository/in-memory.repository";
+import { SortDirection } from "../../../@seadwork/domain/repository/repository-contracts";
 import { Movie } from "../../domain/entities/movie";
 import MovieRepository from "../../domain/repository/movie.repository";
 
 
 export default class MovieInMemoryRepository
     extends InMemorySearchableRepository<Movie>
-    implements MovieRepository{
+    implements MovieRepository.Repository {
+    sortableFields: string[] = ["name", "created_at"];
 
+    protected async applyFilter(
+        items: Movie[], 
+        filter: MovieRepository.Filter
+        ): Promise<Movie[]> {
+        if (!filter) {
+            return items
+        }
+
+        return items.filter((item) => {
+            return (
+                item.props.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()));
+        });
     }
+
+    protected applysort(
+        items: Movie[],
+        sort: string | null,
+        sort_dir: SortDirection | null
+    ): Promise<Movie[]>{
+        return !sort?
+        super.applySort(items, "created_at", "desc")
+        : super.applySort(items, sort, sort_dir)
+        
+    }
+
+}
